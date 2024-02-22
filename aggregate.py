bf8cd60a0cd99476c962ff262d65cee148a95eb1
wilm <frauke.wilm@fau.de>
2024-02-22 20:43:48 +0100

    mIoU aggregation

---
+++ aggregate.py
@@ -0,0 +1,29 @@
+import os
+import json
+import numpy as np
+import argparse 
+import pandas as pd
+
+ 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--experiment_dir", help="Define experiment directory.")
+    args = parser.parse_args()
+    ious = [] #pd.DataFrame(columns=['network', 'loss', 'fold', 'cs2', 'nz20', 'nz210', 'gt450', 'p1000'])
+    runs = filter(lambda file: file.startswith('mi-segnet'), os.listdir(args.experiment_dir))
+    for run in list(runs):
+        with open(os.path.join(args.experiment_dir, run, 'files', "wandb-metadata.json"), 'r') as stream:
+            config = json.load(stream)
+        with open(os.path.join(args.experiment_dir, run, 'files', "iou.json"), 'r') as stream:
+            iou = json.load(stream)
+        iou = {key: np.round(float(iou[key]['mIoU']),2) for key in iou.keys()}
+        iou['loss'] = config['args'][1].split('config_')[-1]
+        iou['network'] = config['args'][5]
+        iou['fold'] = config['args'][7]
+        ious.append(iou)
+    df = pd.DataFrame(ious, columns=['network', 'loss', 'fold', 'cs2', 'nz20', 'nz210', 'gt450', 'p1000'])
+    df.to_csv(os.path.join(args.experiment_dir, 'mIoUs.csv'), index=False)
+        
+
+
+
